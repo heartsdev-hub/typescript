9d73b6d486b5cf98c286aff3238d76879fdd011b
Jack Perez <heartsdev2399@gmail.com>
2026-09-22 20:53:18 -0500

    ts variables

---
+++ public/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let movie = 'El señor de los anillos';
+let hasOscar = true;
+console.log(movie);
+console.log(hasOscar);
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+let movie: string = 'El señor de los anillos';
+let hasOscar: boolean = true;
+
+console.log(movie);
+console.log(hasOscar);
